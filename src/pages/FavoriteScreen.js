11d90f6153c4004fa7134439b8115921fce3f107
Bruno Frigeri <bpiraja97@gmail.com>
2019-12-17 10:28:24 -0300

    Ambient already set up with routes file and tab navigation functional

---
+++ src/pages/FavoriteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const FavoriteScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>FAVORITOS</Text>
+    </SafeAreaView>
+  );
+};
+
+export default FavoriteScreen;
